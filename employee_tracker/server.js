d09001f18dc3710416e9d974b2fc2d33eeaec3ee
Dan <wattierdan@live.com>
2020-12-31 17:31:53 -0800

    adds exit function, refactors code, addEmployee function added aswell

--- employee_tracker/server.js
+++ employee_tracker/server.js
@@ -3,23 +3,21 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
-
-
   port: 3306,
-
   user: "root",
-
   password: "Edwink1225",
   database: "employee_tracker_db"
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-
-  start();
+  if (err) throw err
+  console.log("Welcome to the Employee Tracker. One moment Please...")
+  start()
 });
 
 function start() {
+  setTimeout(function(){
+    console.log("---------------------------------------------------")
     inquirer
       .prompt({
         name: "start",
@@ -27,8 +25,9 @@ function start() {
         message: "What would you like to do?",
         choices: [
             "View All Employees", 
-            "View All Departments", 
-            "View All Roles",
+            "View All Employees by Department", 
+            "View All Employees by Role",
+            "View All Employees by Manager",
             "Add Employee",
             "Add Department",
             "Add Role",
@@ -36,86 +35,144 @@ function start() {
             "Remove Department",
             "Remove Role",
             "Update Employee Role", 
-            "Update Employee Manager"
+            "Update Employee Manager",
+            "----Exit----"
         ]
       })
-      
       .then(function(answer) {
         switch (answer.start){
             case "View All Employees":
-            viewAllEmployees()
-            break
+              viewAllEmployees()
+              break
 
-            case "View All Departments":
-            viewAllDepartments()
-            break
+            case "View All Employees by Department":
+              viewByDepartments()
+              break
 
-            case "View All Roles":
-            viewAllRoles()
-            break
+            case "View All Employees by Role":
+              viewByRole()
+              break
+
+            case "View All Employees by Manager":
+              viewbyManager()
+              break
 
             case "Add Employee":
-            addEmployee()
-            break
+              addEmployee()
+              break
 
             case "Add Department":
-            addDepartment()
-            break
+              addDepartment()
+              break
 
             case "Add Role":
-            addRole()
-            break
+              addRole()
+              break
 
             case "Remove Employee":
-            removeEmployee()
-            break
+              removeEmployee()
+              break
             
             case"Remove Department":
-            removeDepartment()
-            break
+              removeDepartment()
+              break
 
             case "Remove Role":
-            removeRole()
-            break
+              removeRole()
+              break
 
             case "Update Employee Role":
-            updateEmployeeRole()
-            break
+              updateEmployeeRole()
+              break
 
             case "Update Employee Manager":
-            updateEmployeeManager()
-            break
+              updateEmployeeManager()
+              break
+
+            case "----Exit----":
+              console.log("Goodbye...")
+              connection.end()
+              break
         }
     })
-  }
-
-  function viewAllEmployees(){
-        connection.query(
-            `SELECT 
-                CONCAT(e.first_name," ", e.last_name) 
-                AS 'employee', title, salary, 
-                CONCAT(m.first_name," ",m.last_name) 
-                AS manager
-                FROM
-                employee e
-                INNER JOIN employee m ON m.id = e.manager_id 
-                LEFT JOIN role on e.role_id = role.id 
-                ORDER BY manager; `, function(err,res){
-                    if (err) throw err
-                    console.table(res)
-                    start()
-        })       
-  }
-
+  },3000)
+}
 
   //array of departments
   const departments = ['Marketing', 'Finance', 'Operations Management', 'Human Resources', 'IT']
+
+  //array of roles
+  const roles = ["President of Librarians", "Doctor", "Project Manager", "Dog Trainer", "Janitor"]
 
   //vars to temp hold a response
   var changethislater = ""
   var anotherId = ""
 
-  function viewAllDepartments(){
+  //array of questions for addRole function
+  const roleQuestions = [
+    {
+      name:"role_title",
+      type: "input",
+      message: "What is the title of the role you would like to add?"
+    },
+    {
+      name:"role_salary",
+      type: "number",
+      message: "What is the salary for this role?"
+    },
+    {
+      name:"department_id",
+      type: "number",
+      message: "What is the department Id for this role?"
+    }
+  ]  
+
+  //array of questions for addEmployee function
+  const addEmployeeQuestions = [
+    {
+      name:"first_name",
+      type: "input",
+      message:"What is the Employees first name?"
+    },
+    {
+      name:"last_name",
+      type: "input",
+      message:"What is the Employees last name?"
+    },
+    {
+      name:"role_id",
+      type: "number",
+      message:"What is the Employees Role ID?"
+    },
+    {
+      name:"manager_id",
+      type: "number",
+      message:"What is the Employees Manager ID?"
+    }
+  ]  
+
+  function viewAllEmployees(){
+    console.log(`Please wait your employee list is being generated...`)
+    setTimeout(function(){
+      connection.query(
+          `SELECT 
+              CONCAT(e.first_name," ", e.last_name) 
+              AS 'employee', title, salary, 
+              CONCAT(m.first_name," ",m.last_name) 
+              AS manager
+              FROM
+              employee e
+              INNER JOIN employee m ON m.id = e.manager_id 
+              LEFT JOIN role on e.role_id = role.id 
+              ORDER BY manager; `, function(err,res){
+                  if (err) throw err
+                  console.table(res)
+      })       
+    },2000)
+      start()
+  }
+
+  function viewByDepartments(){
     inquirer
       .prompt({
         name: "departments",
@@ -138,7 +195,7 @@ function start() {
                   anotherId = res[0].id
                   return anotherId
           })
-        },2000)
+        },1500)
         setTimeout(function(){
           connection.query(
             `SELECT *
@@ -146,17 +203,12 @@ function start() {
                   if (err) throw err
                   console.table(res)     
           })
-        },3000)
-        setTimeout(function(){
-          start()
-        },6100)
+        },2000)
+        start()
       }) 
   }
 
-  //array of roles
-  const roles = ["President of Librarians", "Doctor", "Project Manager", "Dog Trainer", "Janitor"]
-
-  function viewAllRoles() {
+  function viewByRole() {
     inquirer
       .prompt({
         name: "roles",
@@ -170,7 +222,6 @@ function start() {
           `SELECT id FROM employee_tracker_db.role WHERE title = "${answer.roles}";`, function(err,res){
                   if (err) throw err
                   changethislater = res[0].id
-                  console.log(changethislater)
                   return changethislater      
         })
         setTimeout(function(){
@@ -180,14 +231,31 @@ function start() {
                   console.table(res) 
           })
         },2000)
-        setTimeout(function(){
-          start()
-        },6100)
+        start()
       }) 
   }
 
-  function addEmployee() {
+  function viewbyManager() {
+    
+  }
 
+  function addEmployee() {
+    inquirer
+      .prompt(addEmployeeQuestions)
+  
+      .then(function(answer) {
+        if (answer.add_role !== ''){
+          connection.query(`INSERT INTO employee_tracker_db.employee (first_name, last_name, role_id, manager_id)
+          VALUES ("${answer.first_name}",${answer.last_name},${answer.role_id},${answer.manager_id})`, function(err){
+            if (err) throw err
+            console.log(`adding ${answer.first_name} ${answer.last_name} into Role database`)
+            start()
+          })
+        } else {
+            console.log('Please enter a valid Role name.')
+            addEmployee()
+        }
+      }) 
   }
 
   function addDepartment() {
@@ -214,25 +282,6 @@ function start() {
       }) 
   }
 
-  //array of questions for addRole function
-  const roleQuestions = [
-    {
-      name:"role_title",
-      type: "input",
-      message: "What is the title of the role you would like to add?"
-    },
-    {
-      name:"role_salary",
-      type: "number",
-      message: "What is the salary for this role?"
-    },
-    {
-      name:"department_id",
-      type: "number",
-      message: "What is the department Id for this role?"
-    }
-  ]  
-
   function addRole() {
     inquirer
       .prompt(roleQuestions)
@@ -251,8 +300,29 @@ function start() {
             addRole()
         }
       }) 
+  }
+
+  function removeEmployee() {
 
   }
+
+  function removeDepartment() {
+
+  }
+
+  function removeRole() {
+
+  }
+
+  function updateEmployeeRole() {
+
+  }
+
+  function updateEmployeeManager() {
+    
+  }
+
+ 
 
   
   
